6a8099562831ea427dc17d2934ab5a3f2b6306c3
시형 <qkrtlgud95@naver.com>
2025-11-13 20:45:41 +0900

    asd

---
+++ codeKATA/Python/level3/test.py
@@ -0,0 +1,19 @@
+
+# def solution(s):
+#     words = s.split(" ")
+#     out = []
+
+#     for wi in range(len(words)):
+#         w = words[wi]
+#         chars = []
+
+#         for i in range(len(w)):
+#             ch = w[i]
+#             if i % 2 == 0:
+#                 chars.append(ch.upper())
+#             else:
+#                 chars.append(ch.lower())
+#         out.append("".join(chars))
+
+#     return " ".join(out)
+        
